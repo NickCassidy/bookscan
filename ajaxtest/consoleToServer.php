a0364f246e97b8620c0f83d5db31a0af1992e255
NickCassidy <nick@achromat.net>
2017-11-17 21:03:54 +0000

    removed ajaxtest as had renamed it

--- ajaxtest/consoleToServer.php
+++
@@ -1,9 +0,0 @@
-<?php
-//allow CORS request
-header('Access-Control-Allow-Origin: *');
-
-if(isset($_GET['msg'])) {
-    //you can also log to a file or whatever, I just log to standard logs
-    error_log("[CONSOLE.LOG] ".json_decode($_GET['msg'], true));
-}
-?>
